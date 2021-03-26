71a06c1a4bbd55d12d3a1eceeffeb07a2678dcd6
shynome <shynome@gmail.com>
2021-03-26 08:44:12 +0800

    `start` 命令后面可以接一个端口参数了

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
 import * as JWT from "./jwt.ts";
 import { start } from "./server.ts";
 
-if (import.meta.main) {
-  const args = Deno.args;
-  switch (args[0]) {
-    case "encode": {
-      const token = await JWT.encode(args[1], args[2]);
-      console.log(token);
-      break;
-    }
-    case "decode":
-      console.log(await JWT.decode(args[1]));
-      break;
-    case "start":
-      start();
-      break;
-    default:
-      console.log("添加 start 启动服务");
-      break;
+const args = Deno.args;
+switch (args[0]) {
+  case "encode": {
+    const token = await JWT.encode(args[1], args[2]);
+    console.log(token);
+    break;
   }
+  case "decode":
+    console.log(await JWT.decode(args[1]));
+    break;
+  case "start": {
+    const _port = args[1];
+    let port = Number(_port);
+    if (isNaN(port) || port === 0) {
+      port = 8080;
+    }
+    start(port);
+    break;
+  }
+  default:
+    console.log("添加 start 参数启动服务");
+    break;
 }
